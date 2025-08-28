4088fbb428920735b9cea48999553b37d756e1fc
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-08-28 13:32:16 +0500

    created the home page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,26 @@
+import Logo from "@/src/components/logo";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="petsoft logo"
+        width={519}
+        height={472}
+      />
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          use PetSoft to easily keep track of pets under your care.Get life time
+          access for $299.
+        </p>
+      </div>
+    </main>
+  );
 }
